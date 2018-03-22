44905b3a45b455d28ffa11ccfc44b60075a0de40
Shaked Dovrat <shaked.dovrat@gmail.com>
2018-03-22 17:45:40 +0200

    aug params; experimental

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ class Config(object):
         self.image_size = (28, 28, 1)
         self.model_func = dense_net_bc_wide_40  # dense_net_bc_40
         self.model_name = self.model_func.__name__
-        self.lr = 1e-1
+        self.lr = 1e-2
         self.batch_size = 256
         self.epochs = 100
 
@@ -111,13 +111,18 @@ class Model(object):
         return train, val
 
     def _create_data_generators(self):
-        augmentations = {'preprocessing_function': get_random_eraser(v_l=0.0, v_h=1.0),
-                         'rotation_range': 8,
+        # augmentations = {'preprocessing_function': get_random_eraser(v_l=0.0, v_h=1.0),
+        # augmentations = {'rotation_range': 8,
+        #                  'width_shift_range': 0.08,
+        #                  'height_shift_range': 0.08,
+        #                  'shear_range': 0.3,
+        #                  'zoom_range': 0.08,
+        #                  'horizontal_flip': True}
+        augmentations = {'rotation_range': 4,
                          'width_shift_range': 0.08,
                          'height_shift_range': 0.08,
-                         'shear_range': 0.3,
-                         'zoom_range': 0.08,
-                         'horizontal_flip': True}
+                         'shear_range': 0.1,
+                         'zoom_range': 0.08}
         for dataset in ('train', 'val', 'test'):
             curr_aug = augmentations if dataset == 'train' else {}
             generator = ImageDataGenerator(**curr_aug)
