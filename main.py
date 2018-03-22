b7972b810fae88e93ed8da70d720df248fa5a98c
Shaked Dovrat <shaked.dovrat@gmail.com>
2018-03-22 06:40:51 +0200

    augment training set only

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ from cutout_random_erasing.random_eraser import get_random_eraser
 class Config(object):
     def __init__(self):
         self.image_size = (28, 28, 1)
-        self.model_func = dense_net_bc_40#dense_net_bc_wide_40
+        self.model_func = dense_net_bc_40  # dense_net_bc_wide_40
         self.model_name = self.model_func.__name__
         self.lr = 1e-1
         self.batch_size = 256
@@ -111,26 +111,23 @@ class Model(object):
         return train, val
 
     def _create_data_generators(self):
+        augmentations = {'preprocessing_function': get_random_eraser(v_l=0.0, v_h=1.0),
+                         'rotation_range': 8,
+                         'width_shift_range': 0.08,
+                         'height_shift_range': 0.08,
+                         'shear_range': 0.3,
+                         'zoom_range': 0.08,
+                         'horizontal_flip': True}
         for dataset in ('train', 'val', 'test'):
-            generator = ImageDataGenerator(
-                preprocessing_function=get_random_eraser(v_l=0.0, v_h=1.0),
-                rotation_range=8,
-                width_shift_range=0.08,
-                height_shift_range=0.08,
-                shear_range=0.3,
-                zoom_range=0.08,
-                horizontal_flip=True)
-                # zca_whitening=True,
-                # featurewise_center=True)#False)
-                # featurewise_std_normalization=False)#True)
+            curr_aug = augmentations if dataset == 'train' else {}
+            generator = ImageDataGenerator(**curr_aug)
+            # zca_whitening=True,
+            # featurewise_center=True)#False)
+            # featurewise_std_normalization=False)#True)
             generator.fit(self._datasets['train']['x'])  # fit according to train set.
             self._data_generators[dataset] = generator.flow(self._datasets[dataset]['x'],
                                                             self._datasets[dataset]['y'],
                                                             batch_size=self.c.batch_size)
-
-
-# def dummy_model(image_size):
-#     return 0
 
 
 def main():
